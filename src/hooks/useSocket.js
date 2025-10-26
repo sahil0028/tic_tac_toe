762350b8a807bf9626d7c92892fb9965740d2d16
sahil0028 <126693550+sahil0028@users.noreply.github.com>
2025-10-26 13:01:08 +0530

    pushing render backend

--- src/hooks/useSocket.js
+++ src/hooks/useSocket.js
@@ -2,7 +2,9 @@ import { useEffect, useState } from "react";
 
 const useSocket = () => {
     // const url= 'ws://localhost:8080';
-    const url= 'https://tictactoeback.vercel.app/';
+    // const url= 'https://tictactoeback.vercel.app/';
+    // const url= 'https://tictactoeback.netlify.app/';
+    const url= 'https://tic-tac-toe-a8tv.onrender.com';
   const [socket, setSocket] = useState(null);
   useEffect(() => {
     const ws = new WebSocket(url);
